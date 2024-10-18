946e7ad2ea31e76e2bd8c20a89136ba4b490e5e6
Sakinga <alisasakura20@gmail.com>
2024-10-18 19:16:03 +0500

    Initial commit

---
+++ Assets/Scripts/TestLocal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // структура пременной
+    //1 модификатор доступа (private, public, protected)
+    //2 типы данных (int, string, bool, float)
+    //3 имя переменной 
+    
+    //правила наименования переменной
+    //1 Для чего она нужна
+    //2 Не должна содержать символ или цифры
+    //3 Первое с маленькой, второе с большой
+
+    //public int speed = 10;
+    //private string carName = Toyota;
+}
